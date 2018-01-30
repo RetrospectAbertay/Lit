3fb703fa3323f7e7c06d8eaa19ae98ffdce112bd
marc-abertay <marci.philippe@googlemail.com>
2018-01-30 23:58:51 +0000

    added conveyer belt mechanic

---
+++ Plattformer (PP Game 1)/Assets/Standard Assets/2D/Scripts/ConveyerBelt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyerBelt : MonoBehaviour
+{
+
+    [SerializeField] private float m_Force;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public float GetBeltForce()
+    {
+        return m_Force;
+    }
+}
--- Plattformer (PP Game 1)/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Plattformer (PP Game 1)/Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -7,9 +7,9 @@ namespace UnityStandardAssets._2D
     {
         [SerializeField] private float m_MaxSpeed = 10f;                    // The fastest the player can travel in the x axis.
         [SerializeField] private float m_JumpForce = 400f;                  // Amount of force added when the player jumps.
-        [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
         [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
         [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
+        [SerializeField] private float m_ConveyerForce = 20.0f;
 
         private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
         const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
@@ -18,6 +18,7 @@ namespace UnityStandardAssets._2D
         const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
         private Rigidbody2D m_Rigidbody2D;
         private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+        private float m_beltForce = 0.0f;
 
         private void Awake()
         {
@@ -38,44 +39,47 @@ namespace UnityStandardAssets._2D
             for (int i = 0; i < colliders.Length; i++)
             {
                 if (colliders[i].gameObject != gameObject)
+                {
                     m_Grounded = true;
+                    // check if the player is on a conveyer belt
+                    if (colliders[i].transform.GetComponent<ConveyerBelt>())
+                    {
+                        // set force of belt to apply to the player
+                        m_beltForce = colliders[i].transform.GetComponent<ConveyerBelt>().GetBeltForce();
+                    }
+                }
             }
         }
 
 
         public void Move(float move, bool crouch, bool jump)
         {
-            // If crouching, check to see if the character can stand up
-            if (!crouch)
+            //only control the player if grounded or airControl is turned on
+            if (m_Grounded)
             {
-                // If the character has a ceiling preventing them from standing up, keep them crouching
-                if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
+                if (m_AirControl)
                 {
-                    crouch = true;
+                    // Move the character
+                    m_Rigidbody2D.velocity = new Vector2(move * m_MaxSpeed + m_beltForce, m_Rigidbody2D.velocity.y);
+
+                    // If the input is moving the player right and the player is facing left...
+                    if (move > 0 && !m_FacingRight)
+                    {
+                        // ... flip the player.
+                        Flip();
+                    }
+                    // Otherwise if the input is moving the player left and the player is facing right...
+                    else if (move < 0 && m_FacingRight)
+                    {
+                        // ... flip the player.
+                        Flip();
+                    }
                 }
             }
-
-            //only control the player if grounded or airControl is turned on
-            if (m_Grounded || m_AirControl)
+            else
             {
-                // Reduce the speed if crouching by the crouchSpeed multiplier
-                move = (crouch ? move*m_CrouchSpeed : move);
-
-                // Move the character
-                m_Rigidbody2D.velocity = new Vector2(move*m_MaxSpeed, m_Rigidbody2D.velocity.y);
-
-                // If the input is moving the player right and the player is facing left...
-                if (move > 0 && !m_FacingRight)
-                {
-                    // ... flip the player.
-                    Flip();
-                }
-                    // Otherwise if the input is moving the player left and the player is facing right...
-                else if (move < 0 && m_FacingRight)
-                {
-                    // ... flip the player.
-                    Flip();
-                }
+                // reset belt force
+                m_beltForce = 0.0f;
             }
             // If the player should jump...
             if (m_Grounded && jump)
@@ -85,7 +89,6 @@ namespace UnityStandardAssets._2D
                 m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
             }
         }
-
 
         private void Flip()
         {
