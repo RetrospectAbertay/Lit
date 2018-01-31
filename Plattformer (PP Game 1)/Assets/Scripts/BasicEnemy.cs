28765656276183ffdb5b6146bd684a8fa81bb774
marc-abertay <marci.philippe@googlemail.com>
2018-01-31 15:50:13 +0000

    added camera movement script

--- Plattformer (PP Game 1)/Assets/Scripts/BasicEnemy.cs
+++ Plattformer (PP Game 1)/Assets/Scripts/BasicEnemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyScript : MonoBehaviour
+public class BasicEnemy : MonoBehaviour
 {
     [SerializeField] private float m_Speed;
     [SerializeField] private float m_PathTime;
---
+++ Plattformer (PP Game 1)/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        // grab the player
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+	}
+}
