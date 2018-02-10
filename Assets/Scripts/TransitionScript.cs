305eb5f4b4c42ad0354a8d307599dc27c851e705
marc-abertay <marci.philippe@googlemail.com>
2018-02-10 16:18:02 +0000

    added new transition scene, added retro font, added transitional script

---
+++ Assets/Scripts/TransitionScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionScript : MonoBehaviour {
+
+    public enum TRANSITLEVEL
+    {
+        FirstLevel,
+        SecondLevel,
+        ThirdLevel,
+        FourthLevel,
+        FifthLevel
+    }
+
+    public TRANSITLEVEL nextLevel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // player press space - go to the next level
+        if (Input.GetKeyDown("space"))
+        {
+            switch(nextLevel)
+            {
+                case TRANSITLEVEL.FirstLevel:
+                    SceneManager.LoadScene("1. T Level");
+                    break;
+                case TRANSITLEVEL.SecondLevel:
+                    SceneManager.LoadScene("2. I Level");
+                    break;
+                case TRANSITLEVEL.ThirdLevel:
+                    SceneManager.LoadScene("3. M Level");
+                    break;
+                case TRANSITLEVEL.FourthLevel:
+                    SceneManager.LoadScene("4. E Level");
+                    break;
+                case TRANSITLEVEL.FifthLevel:
+                    SceneManager.LoadScene("5. X Level");
+                    break;
+            }
+        }
+    }
+}
