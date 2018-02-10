420915b74481da0c9254d93094db1aae9ca9c817
marc-abertay <marci.philippe@googlemail.com>
2018-02-10 16:45:31 +0000

    added indicator used to show what letters have been collected

--- Assets/Scripts/TransitionScript.cs
+++ Assets/Scripts/TransitionScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TransitionScript : MonoBehaviour {
 
@@ -15,11 +16,30 @@ public class TransitionScript : MonoBehaviour {
     }
 
     public TRANSITLEVEL nextLevel;
+    public Text timexLetters;
+
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        switch (nextLevel)
+        {
+            case TRANSITLEVEL.FirstLevel:
+                timexLetters.text = "";
+                break;
+            case TRANSITLEVEL.SecondLevel:
+                timexLetters.text = "T";
+                break;
+            case TRANSITLEVEL.ThirdLevel:
+                timexLetters.text = "T I";
+                break;
+            case TRANSITLEVEL.FourthLevel:
+                timexLetters.text = "T I M";
+                break;
+            case TRANSITLEVEL.FifthLevel:
+                timexLetters.text = "T I M E";
+                break;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -30,18 +50,23 @@ public class TransitionScript : MonoBehaviour {
             {
                 case TRANSITLEVEL.FirstLevel:
                     SceneManager.LoadScene("1. T Level");
+                    timexLetters.text = "";
                     break;
                 case TRANSITLEVEL.SecondLevel:
                     SceneManager.LoadScene("2. I Level");
+                    timexLetters.text = "T";
                     break;
                 case TRANSITLEVEL.ThirdLevel:
                     SceneManager.LoadScene("3. M Level");
+                    timexLetters.text = "T I";
                     break;
                 case TRANSITLEVEL.FourthLevel:
                     SceneManager.LoadScene("4. E Level");
+                    timexLetters.text = "T I M";
                     break;
                 case TRANSITLEVEL.FifthLevel:
                     SceneManager.LoadScene("5. X Level");
+                    timexLetters.text = "T I M E";
                     break;
             }
         }
