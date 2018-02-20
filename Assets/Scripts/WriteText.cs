80e29fd3da4c5f87ffccd5f040743f2dc23f44a4
marc-abertay <marci.philippe@googlemail.com>
2018-02-20 13:29:45 +0000

    started working on script that displays text like in the spectrum, got string conversion from text object

---
+++ Assets/Scripts/WriteText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WriteText : MonoBehaviour {
+
+    Text textObject;
+    string curDisplay;
+    float writeSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // get the contents of the string
+        curDisplay = this.GetComponent<Text>().text;
+        Debug.Log(curDisplay);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
