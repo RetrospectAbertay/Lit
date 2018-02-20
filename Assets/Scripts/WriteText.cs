772426c23fd707a627f7e1aeba9c23a46ec10d6a
marc-abertay <marci.philippe@googlemail.com>
2018-02-20 13:52:04 +0000

    text now being created procedurally, looking spectrum-esque

--- Assets/Scripts/WriteText.cs
+++ Assets/Scripts/WriteText.cs
@@ -6,20 +6,58 @@ using UnityEngine.UI;
 public class WriteText : MonoBehaviour {
 
     Text textObject;
-    string curDisplay;
-    float writeSpeed;
+    string stringToDisplay;
+    public float writeSpeed;
+    float timer;
+    int curChar = 0;
 
 	// Use this for initialization
 	void Start ()
     {
+        // get the text object iself
+        textObject = this.GetComponent<Text>();
         // get the contents of the string
-        curDisplay = this.GetComponent<Text>().text;
-        Debug.Log(curDisplay);
+        stringToDisplay = textObject.text;
+        Debug.Log(stringToDisplay);
+        // setup timer to init to 0
+        timer = 0.0f;
+        // display empty text
+        updateText();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        // check if we still need to write
+		if(curChar < stringToDisplay.Length)
+        {
+            Debug.Log("updating timer");
+            // increment timer
+            timer += Time.deltaTime;
+            if(timer > writeSpeed)
+            {
+                // increment cur char
+                curChar++;
+                // display a new character
+                updateText();
+                // reset timer
+                timer = 0.0f;
+            }
+        }
 	}
+
+    void updateText()
+    {
+        // initialise new string
+        string curText = "";
+        // iterate through the initial message
+        for(int i = 0; i < curChar; i++)
+        {
+            // add each char to the current text
+            curText += stringToDisplay[i];
+        }
+        Debug.Log(curText);
+        // update text object
+        textObject.text = curText;
+    }
 }
