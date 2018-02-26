75df36b5f737de7ba648f4a5c7ee38e3e51a347a
marc-abertay <marci.philippe@googlemail.com>
2018-02-26 12:27:44 +0000

    animation now working well

--- Assets/Scripts/AnimationPlayer.cs
+++ Assets/Scripts/AnimationPlayer.cs
@@ -17,16 +17,17 @@ public class AnimationPlayer : MonoBehaviour
     public Sprite[] FallingAnimation;
     private Sprite[] curAnimation;
     public float FrameDuration;
-    private int frameCounter;
-    private float timer;
+    private int frameCounter = 0;
+    private float timer = 0.0f;
     private SpriteRenderer spriteRenderer;
 
 	// Use this for initialization
 	void Start ()
 	{
         // get sprite renderer component
-	    spriteRenderer = GetComponent<SpriteRenderer>();
-	}
+	    spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+        curAnimation = IdleAnimation;
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -36,32 +37,36 @@ public class AnimationPlayer : MonoBehaviour
         // if the timer exceeds the predefined frame time, we want to play the next frame
 	    if (timer > FrameDuration)
 	    {
+            // allocate the sprite renderer to display the frame correllating to the frame counter
+            spriteRenderer.sprite = curAnimation[frameCounter];
             // increment frame counter
-	        frameCounter++;
+            frameCounter++;
+            Debug.Log(frameCounter);
             // check that our frame counter has not exceeded the maximum number of frames in our animation
-	        if (frameCounter < curAnimation.Length)
-	        {
-	            // do nothing if frame counter is accessing a viable frame
-	        }
-	        else
+	        if (frameCounter > (curAnimation.Length - 1))
 	        {
                 // otherwise we want to reset our frame counter, which will display the first frame
-	            frameCounter = 0;
-	        }
-	        // allocate the sprite renderer to display the frame correllating to the frame counter
-	        spriteRenderer.sprite = curAnimation[frameCounter];
+                frameCounter = 0;
+                Debug.Log("reseting frame counter");
+            }
             // reset the timer for the next update
 	        timer = 0.0f;
 	    }
 	}
 
+    public void PlayWalkAnimation()
+    {
+
+    }
+
     public void ChangeAnimation(AnimationState newAnimation)
     {
-        // switch statement checks for each possible animation state and allocates the right one
+        //// switch statement checks for each possible animation state and allocates the right one
         switch (newAnimation)
         {
             case AnimationState.IDLE:
                 curAnimation = IdleAnimation;
+                frameCounter = 0;
                 break;
             case AnimationState.JUMPING:
                 curAnimation = JumpingAnimation;
