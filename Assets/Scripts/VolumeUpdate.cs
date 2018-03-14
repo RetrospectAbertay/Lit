c00556dd872bc4c0213df520987466c22ea7bbc9
marc-abertay <marci.philippe@googlemail.com>
2018-03-14 16:34:48 +0000

    volume options are now applied to the entire game

---
+++ Assets/Scripts/VolumeUpdate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeUpdate : MonoBehaviour {
+
+    public bool musicSource;
+
+	// Use this for initialization
+	void Start () {
+        if (musicSource)
+        {
+            this.GetComponent<AudioSource>().volume = (float)(0.2 * PlayerPrefs.GetInt("Music Volume"));
+        }
+        else
+        {
+            this.GetComponent<AudioSource>().volume = (float)(0.2 * PlayerPrefs.GetInt("Sound Volume"));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
